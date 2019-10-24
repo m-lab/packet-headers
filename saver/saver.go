78e23fa95cafd885dc9e0bc156ba8f6c4a67d987
Peter Boothe <pboothe@google.com>
2019-10-24 17:00:56 -0400

    Adds the saver.  100% unit tested.

--- saver/saver.go
+++ saver/saver.go
@@ -5,17 +5,55 @@ import (
 	"log"
 	"os"
 	"path"
+	"sync"
 	"time"
 
-	"github.com/m-lab/packet-headers/metrics"
+	"github.com/m-lab/go/anonymize"
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pcapgo"
+	"github.com/m-lab/packet-headers/metrics"
 )
 
-func filename(dir, uuid string, t time.Time) (string, string) {
-	return path.Join(dir, t.Format("2006/01/02")), uuid + ".pcap"
+// A nice example of why go generics might be nice sometimes.
+func minInt(x, y int) int {
+	if x <= y {
+		return x
+	}
+	return y
+}
+
+// anonymize a packet by modifying its contained IP addresses in-place.
+func anonymizePacket(a anonymize.IPAnonymizer, p gopacket.Packet) {
+	if p == nil || a == nil {
+		log.Println("Null packet or anonymizer. Can not anonymizePacket")
+		return
+	}
+	nl := p.NetworkLayer()
+	if nl == nil {
+		log.Println("Packets with no network layers should never make it here")
+		return
+	}
+	switch nl.LayerType() {
+	case ((*layers.IPv4)(nil)).LayerType():
+		a.IP(nl.(*layers.IPv4).SrcIP)
+		a.IP(nl.(*layers.IPv4).DstIP)
+	case ((*layers.IPv6)(nil)).LayerType():
+		a.IP(nl.(*layers.IPv6).SrcIP)
+		a.IP(nl.(*layers.IPv6).DstIP)
+	}
+}
+
+// UUIDEvent is passed to the saver along with an event arrival timestamp so
+// that the saver can produce an appropriately-named pcap file.
+type UUIDEvent struct {
+	UUID      string
+	Timestamp time.Time
+}
+
+func filename(dir string, e UUIDEvent) (string, string) {
+	return path.Join(dir, e.Timestamp.Format("2006/01/02")), e.UUID + ".pcap"
 }
 
 type statusSetter interface {
@@ -39,74 +77,110 @@ func (s *status) Get() string {
 }
 
 // Saver provides two channels to allow packets to be saved. A well-buffered
-// channel for packets and a channel to receive the UUID string.
+// channel for packets and a channel to receive the UUID.
 type Saver struct {
-	Pchan    chan gopacket.Packet
-	UUIDchan chan string
+	// Pchan is the channel down which pointers to packets will be sent. No
+	// client should ever perform a blocking write to this channel, and if a
+	// client holds onto the channel for longer than a millisecond, it risks the
+	// channel being closed out from underneath it. Nothing that uses the Pchan
+	// channel of a Saver should hold onto that channel for longer than it takes
+	// to attempt to write a single record.
+	Pchan    chan<- gopacket.Packet
+	UUIDchan chan<- UUIDEvent
+
+	// The internal-only readable channels. Until the Saver is stopped, the
+	// Pchan write channel is connected to the Pchan channel. After things are
+	// stopped, that connection is broken.
+	pchanRead    <-chan gopacket.Packet
+	uuidchanRead <-chan UUIDEvent
 
 	dir    string
 	cancel func()
 	state  statusSetter
+	anon   anonymize.IPAnonymizer
+
+	stopOnce sync.Once
+}
+
+// Increment the error counter when errors are encountered.
+func (s *Saver) error(cause string) {
+	s.state.Set(cause + "error")
+	metrics.SaverErrors.WithLabelValues(cause).Inc()
 }
 
 // Start the process of reading the data and saving it to a file.
-func (s *Saver) Start(ctx context.Context, duration time.Duration) {
+func (s *Saver) start(ctx context.Context, duration time.Duration) {
 	metrics.SaversStarted.Inc()
 	defer metrics.SaversStopped.Inc()
+	defer s.state.Set("stopped")
 
 	ctx, s.cancel = context.WithTimeout(ctx, duration)
-	defer s.Stop()
+	defer s.cancel()
+	go func() {
+		<-ctx.Done()
+		s.Stop()
+	}()
 
 	// First read the UUID
 	s.state.Set("uuidwait")
-	var uuid string
+	var uuidEvent UUIDEvent
 	select {
-	case uuid = <-s.UUIDchan:
+	case uuidEvent = <-s.uuidchanRead:
 	case <-ctx.Done():
 		log.Println("PCAP capture cancelled with no UUID")
-		metrics.SaverErrors.WithLabelValues("uuid").Inc()
-		s.state.Set("uuiderror")
+		s.error("uuid")
 		return
 	}
 
 	// Create a file and directory based on the UUID and the time.
 	s.state.Set("filecreation")
-	path, fname := filename(s.dir, uuid, time.Now())
-	err := os.MkdirAll(path, 0777)
+	dir, fname := filename(s.dir, uuidEvent)
+	err := os.MkdirAll(dir, 0777)
 	if err != nil {
-		log.Println("Could not create directory", path, err)
-		metrics.SaverErrors.WithLabelValues("mkdir").Inc()
-		s.state.Set("mkdirerror")
+		log.Println("Could not create directory", dir, err)
+		s.error("mkdir")
 		return
 	}
-	f, err := os.Create(path + fname)
+	f, err := os.Create(path.Join(dir, fname))
 	if err != nil {
-		log.Println("Could not create file", path, fname, err)
-		metrics.SaverErrors.WithLabelValues("create").Inc()
-		s.state.Set("createerror")
+		s.error("create")
 		return
 	}
 	defer f.Close()
 
 	// Write PCAP data to the new file.
 	w := pcapgo.NewWriterNanos(f)
-	// TODO: find a better value than 1600
-	w.WriteFileHeader(1600, layers.LinkTypeEthernet)
 	// Now save packets until the stream is done or the context is canceled.
 	s.state.Set("readingpackets")
-	for {
-		select {
-		case p, ok := <-s.Pchan:
-			if !ok {
-				return
-			}
-			// TODO: zero out all non-header packet bytes
-			// TODO: apply anonymization
-			w.WritePacket(p.Metadata().CaptureInfo, p.Data())
-		case <-ctx.Done():
-			return
-		}
+	// Read the first packet to determine the TCP+IP header size (as IPv6 is variable in size)
+	p, ok := <-s.pchanRead
+	if !ok {
+		s.error("nopackets")
+		return
 	}
+	headerLen := len(p.Data())
+	// Now we try to discover the correct header length for the flow by
+	// discovering the size of the application layer and then subtracting it
+	// from the overall size of the packet data. IPv6 supports variable-length
+	// headers, so this is actually required.
+	al := p.ApplicationLayer()
+	if al != nil {
+		alSize := len(al.LayerContents())
+		headerLen -= alSize
+	}
+	// Write out the header and the first packet.
+	w.WriteFileHeader(uint32(headerLen), layers.LinkTypeEthernet)
+	s.savePacket(w, p, headerLen)
+	for p := range s.pchanRead {
+		s.savePacket(w, p, headerLen)
+	}
+}
+
+func (s *Saver) savePacket(w *pcapgo.Writer, p gopacket.Packet, headerLen int) {
+	info := p.Metadata().CaptureInfo
+	info.CaptureLength = minInt(info.CaptureLength, headerLen)
+	anonymizePacket(s.anon, p)
+	w.WritePacket(info, p.Data()[:headerLen])
 }
 
 // Stop the saver, causing it to write its data to disk and close all open
@@ -115,13 +189,24 @@ func (s *Saver) Start(ctx context.Context, duration time.Duration) {
 // the garbage collector will close all open channels and reclaim all the
 // resources of this saver.
 func (s *Saver) Stop() {
-	s.state.Set("stopped")
-	if s.cancel != nil {
-		s.cancel()
-	}
-	// Zero out the channel capacity - buffering data is foolish when it will
-	// never be read.
-	s.Pchan = make(chan gopacket.Packet)
+	s.stopOnce.Do(func() {
+		if s.cancel != nil {
+			s.cancel()
+		}
+		oldchan := s.Pchan
+		// All future writes should go to a channel that has no capacity and will
+		// never be read from.
+		s.Pchan = make(chan gopacket.Packet)
+		go func() {
+			// Lose all race conditions, because saver requires that nobody hold
+			// onto the Pchan channel for more than a millisecond.
+			time.Sleep(time.Millisecond)
+
+			// Tell the packet channel that the currently buffered data is all there
+			// will ever be.
+			close(oldchan)
+		}()
+	})
 }
 
 // State returns the state of the saver in a form suitable for use as a label
@@ -130,25 +215,39 @@ func (s *Saver) State() string {
 	return s.state.Get()
 }
 
-// New creates a new Saver to save a single TCP flow.
-func New(dir string) *Saver {
+// newSaver makes a new Saver but does not start it. It is here as its own
+// function to enable whitebox testing and instrumentation.
+func newSaver(dir string, anon anonymize.IPAnonymizer) *Saver {
 	beginstate := "notstarted"
 	metrics.SaverCount.WithLabelValues(beginstate).Inc()
-	return &Saver{
-		// With a 1500 byte MTU, this is a three second buffer at
-		// a line rate of 10Gbps:
-		// 10000000 bits/second * 3 seconds * 1/8 bytes/bit * 1/1500 packets/byte = 2500 packets
-		//
-		// If synchronization between UUID creation and packet
-		// collection is off by more than three seconds, things are
-		// messed up.
-		Pchan: make(chan gopacket.Packet, 2500),
 
-		// There should only ever be (at most) one write to the UUIDchan, so a
-		// capacity of 1 means that the write should never block.
-		UUIDchan: make(chan string, 1),
+	// With a 1500 byte MTU, this is a ~1 second buffer at a line rate of 10Gbps:
+	// 10e9 bits/second * 1 second * 1/8 bytes/bit * 1/1500 packets/byte = 833333.3 packets
+	//
+	// If synchronization between UUID creation and packet collection is off by
+	// more than a second, things are messed up.
+	pchan := make(chan gopacket.Packet, 833333)
+
+	// There should only ever be (at most) one write to the UUIDchan, so a
+	// capacity of 1 means that the write should never block.
+	uuidchan := make(chan UUIDEvent, 1)
+
+	return &Saver{
+		Pchan:     pchan,
+		pchanRead: pchan,
+
+		UUIDchan:     uuidchan,
+		uuidchanRead: uuidchan,
 
 		dir:   dir,
 		state: &status{beginstate},
+		anon:  anon,
 	}
+}
+
+// StartNew creates a new Saver to save a single TCP flow and starts its goroutine.
+func StartNew(ctx context.Context, anon anonymize.IPAnonymizer, dir string, maxDuration time.Duration) *Saver {
+	s := newSaver(dir, anon)
+	go s.start(ctx, maxDuration)
+	return s
 }
--- saver/saver_test.go
+++ saver/saver_test.go
@@ -2,14 +2,69 @@ package saver
 
 import (
 	"context"
+	"encoding/hex"
 	"io/ioutil"
+	"log"
+	"net"
 	"os"
 	"reflect"
 	"testing"
 	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+	"github.com/m-lab/go/anonymize"
 	"github.com/m-lab/go/rtx"
 )
+
+func TestAnonymization(t *testing.T) {
+	a := anonymize.New(anonymize.Netblock)
+
+	// Try to anonymize packets with no IP data.
+	anonymizePacket(a, nil) // No crash == success
+	buf := gopacket.NewSerializeBuffer()
+	opts := gopacket.SerializeOptions{} // See SerializeOptions for more details.
+	gopacket.SerializeLayers(buf, opts,
+		&layers.Ethernet{
+			SrcMAC: net.HardwareAddr("123456"),
+			DstMAC: net.HardwareAddr("654321"),
+		},
+	)
+	ethOnlyPacket := gopacket.NewPacket(
+		buf.Bytes(),
+		layers.LayerTypeEthernet,
+		gopacket.Default)
+	anonymizePacket(a, ethOnlyPacket) // No crash == success
+
+	// Again, but now with actual IPv4 data in the packet.
+	buf = gopacket.NewSerializeBuffer()
+	ipv4 := &layers.IPv4{}
+	// Hand-modified packet data from wireshark for 10.101.236.44 -> 10.211.118.21
+	// IPs have been changed to prevent exposure of internal IPs.
+	ipv4bytes, err := hex.DecodeString("4500006c489b4000400618770a65ec2c0ad37615")
+	rtx.Must(err, "Could not decode byte string")
+	rtx.Must(ipv4.DecodeFromBytes(ipv4bytes, gopacket.NilDecodeFeedback), "Could not reify v4 packet")
+	gopacket.SerializeLayers(buf, opts, ipv4)
+	ipv4Packet := gopacket.NewPacket(
+		buf.Bytes(),
+		layers.LayerTypeIPv4,
+		gopacket.Default)
+	if ipv4Packet.NetworkLayer().(*layers.IPv4).SrcIP.String() != "10.101.236.44" {
+		t.Error("Failed to reify srcIP:", ipv4Packet.NetworkLayer().(*layers.IPv4).SrcIP.String())
+	}
+	if ipv4Packet.NetworkLayer().(*layers.IPv4).DstIP.String() != "10.211.118.21" {
+		t.Error("Failed to reify dstIP:", ipv4Packet.NetworkLayer().(*layers.IPv4).DstIP.String())
+	}
+	// Perform the anonymization
+	anonymizePacket(a, ipv4Packet)
+	if ipv4Packet.NetworkLayer().(*layers.IPv4).SrcIP.String() != "10.101.236.0" {
+		t.Error("Failed to anonymize srcIP:", ipv4Packet.NetworkLayer().(*layers.IPv4).SrcIP.String())
+	}
+	if ipv4Packet.NetworkLayer().(*layers.IPv4).DstIP.String() != "10.211.118.0" {
+		t.Error("Failed to anonymize dstIP:", ipv4Packet.NetworkLayer().(*layers.IPv4).DstIP.String())
+	}
+}
 
 type statusTracker struct {
 	status string
@@ -28,16 +83,19 @@ func (s *statusTracker) Get() string {
 	return s.status
 }
 
-func TestStatusDryRun(t *testing.T) {
-	dir, err := ioutil.TempDir("", "TestStatus")
+func TestSaverDryRun(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestSaverDryRun")
 	rtx.Must(err, "Could not create tempdir")
-	defer os.RemoveAll(dir)
+	//defer os.RemoveAll(dir)
 
-	s := New(dir)
+	s := newSaver(dir, anonymize.New(anonymize.None))
 	tracker := statusTracker{status: s.state.Get()}
 	s.state = &tracker
 
-	s.UUIDchan <- "testUUID"
+	tstamp := time.Date(2000, 1, 2, 3, 4, 5, 6, time.UTC)
+
+	// Send a UUID but never send any packets.
+	s.UUIDchan <- UUIDEvent{"testUUID", tstamp}
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
@@ -49,15 +107,155 @@ func TestStatusDryRun(t *testing.T) {
 		s.Stop()
 	}()
 
-	s.Start(context.Background(), 10*time.Second) // Give the disk IO 10 seconds to happen.
+	s.start(ctx, 10*time.Second) // Give the disk IO 10 seconds to happen.
 	expected := statusTracker{
 		status: "stopped",
-		past:   []string{"notstarted", "uuidwait", "filecreation", "readingpackets"},
+		past:   []string{"notstarted", "uuidwait", "filecreation", "readingpackets", "nopacketserror"},
 	}
 	if !reflect.DeepEqual(&tracker, &expected) {
 		t.Errorf("%+v != %+v", &tracker, &expected)
 	}
 	if s.State() != "stopped" {
 		t.Errorf("%s != 'stopped'", s.State())
+	}
+}
+
+func TestSaverNoUUID(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestSaverNoUUID")
+	rtx.Must(err, "Could not create tempdir")
+	defer os.RemoveAll(dir)
+
+	s := newSaver(dir, anonymize.New(anonymize.None))
+	tracker := statusTracker{status: s.state.Get()}
+	s.state = &tracker
+
+	s.start(context.Background(), 10*time.Millisecond)
+	expected := statusTracker{
+		status: "stopped",
+		past:   []string{"notstarted", "uuidwait", "uuiderror"},
+	}
+	if !reflect.DeepEqual(&tracker, &expected) {
+		t.Errorf("%+v != %+v", &tracker, &expected)
+	}
+	if s.State() != "stopped" {
+		t.Errorf("%s != 'stopped'", s.State())
+	}
+}
+
+func TestSaverCantMkdir(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestSaverCantMkdir")
+	rtx.Must(err, "Could not create tempdir")
+	rtx.Must(os.Chmod(dir, 0111), "Could not chmod dir to unwriteable")
+	defer os.RemoveAll(dir)
+
+	s := newSaver(dir, anonymize.New(anonymize.None))
+	tracker := statusTracker{status: s.state.Get()}
+	s.state = &tracker
+
+	s.UUIDchan <- UUIDEvent{"testUUID", time.Now()}
+	s.start(context.Background(), 10*time.Second)
+
+	expected := statusTracker{
+		status: "stopped",
+		past:   []string{"notstarted", "uuidwait", "filecreation", "mkdirerror"},
+	}
+	if !reflect.DeepEqual(&tracker, &expected) {
+		t.Errorf("%+v != %+v", &tracker, &expected)
+	}
+	if s.State() != "stopped" {
+		t.Errorf("%s != 'stopped'", s.State())
+	}
+}
+
+func TestSaverCantCreate(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestSaverCantCreate")
+	rtx.Must(err, "Could not create tempdir")
+	defer os.RemoveAll(dir)
+
+	s := newSaver(dir, anonymize.New(anonymize.None))
+	tracker := statusTracker{status: s.state.Get()}
+	s.state = &tracker
+
+	// A UUID containing a non-shell-safe character will never happen in
+	// practice, but it does cause the resulting file to fail in os.Create()
+	s.UUIDchan <- UUIDEvent{"test/UUID", time.Now()}
+	s.start(context.Background(), 10*time.Second)
+
+	expected := statusTracker{
+		status: "stopped",
+		past:   []string{"notstarted", "uuidwait", "filecreation", "createerror"},
+	}
+	if !reflect.DeepEqual(&tracker, &expected) {
+		t.Errorf("%+v != %+v", &tracker, &expected)
+	}
+	if s.State() != "stopped" {
+		t.Errorf("%s != 'stopped'", s.State())
+	}
+}
+
+func TestSaverWithRealData(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestSaverWithRealData")
+	rtx.Must(err, "Could not create tempdir")
+	defer os.RemoveAll(dir)
+
+	// Send a UUID and then some packets.
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	s := StartNew(ctx, anonymize.New(anonymize.Netblock), dir, 10*time.Second)
+
+	tstamp := time.Date(2000, 1, 2, 3, 4, 5, 6, time.UTC)
+	s.UUIDchan <- UUIDEvent{"testUUID", tstamp}
+
+	go func() {
+		// Get packets from a wireshark-produced pcap file.
+		handle, err := pcap.OpenOffline("test.pcap")
+		rtx.Must(err, "Could not open golden pcap file")
+		ps := gopacket.NewPacketSource(handle, handle.LinkType())
+		// Send packets down the packet channel
+		for p := range ps.Packets() {
+			s.Pchan <- p
+		}
+		// Stop the saver.
+		s.Stop()
+	}()
+
+	s.start(ctx, 10*time.Second)
+	log.Println("reading data from", dir+"/2000/01/02/testUUID.pcap")
+	handle, err := pcap.OpenOffline(dir + "/2000/01/02/testUUID.pcap")
+	rtx.Must(err, "Could not open written pcap file")
+	ps := gopacket.NewPacketSource(handle, handle.LinkType())
+	var packets []gopacket.Packet
+	for p := range ps.Packets() {
+		// Verify that each packet has had its payload zeroed out.
+		packets = append(packets, p)
+	}
+	if len(packets) != 4 {
+		t.Error("Bad length (should be 4):", len(packets))
+	}
+	for _, p := range packets {
+		al := p.ApplicationLayer()
+		if al == nil {
+			continue
+		}
+		data := al.LayerContents()
+		for _, b := range data {
+			if b != 0 {
+				t.Error("All application layer data is supposed to be zeroed, but was not in", p)
+				break
+			}
+		}
+		srcIP := p.NetworkLayer().(*layers.IPv6).SrcIP
+		for b := range srcIP[16:] {
+			if b != 0 {
+				t.Error("All high-end v6 address bytes should be zeroed out")
+			}
+		}
+		dstIP := p.NetworkLayer().(*layers.IPv6).DstIP
+		for b := range dstIP[16:] {
+			if b != 0 {
+				t.Error("All high-end v6 address bytes should be zeroed out")
+			}
+		}
 	}
 }
