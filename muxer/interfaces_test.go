a086e47b485e8a526154885846bb1a0f29283738
Peter Boothe <pboothe@google.com>
2019-10-31 14:22:51 -0400

    Corrected comments in one test case.

--- muxer/interfaces_test.go
+++ muxer/interfaces_test.go
@@ -64,14 +64,15 @@ func TestMuxPacketsUntilContextCancellation(t *testing.T) {
 		time.Sleep(100 * time.Millisecond)
 		cancel()
 	}()
-	// The only way that this will close is if we exhaust all the input channels
-	// and they each close. So let's do that.
+	// The input channels will never close, so only context cancellation will work.
 	pcount := 0
 	for range out {
 		pcount++
 	}
 	wg.Wait()
-	// Verify that the combined flow contains the right number of packets.
+	// If we got to here, then muxPackets terminated!  Hooray!
+
+	// Verify that the combined flow contained no packets.
 	if pcount != 0 {
 		t.Errorf("pcount should be 0, not %d", pcount)
 	}
