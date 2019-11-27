415686b478e4025fe2b052bdf90b82e268dcba2c
Stephen Soltesz <soltesz@google.com>
2019-11-27 15:42:05 -0500

    Update comment

--- saver/tcp.go
+++ saver/tcp.go
@@ -211,7 +211,7 @@ func (t *TCP) savePackets(ctx context.Context, uuidDelay, duration time.Duration
 	}
 	// uuidEvent is now set to a good value.
 
-	// Continue reading packets until duration seconds.
+	// Continue reading packets until duration has elapsed.
 	for {
 		p, ok := t.readPacket(derivedCtx)
 		if !ok {
